97168ca966af9c52443ead3867222204edee1266
ROVIC ROSALES <ritzy102799@gmail.com>
2024-02-20 18:43:55 +0100

    made the code more readable

--- MSLearn.CSharpReadableCode.Challenge/Program.cs
+++ MSLearn.CSharpReadableCode.Challenge/Program.cs
@@ -1,13 +1,26 @@
-﻿string str = "The quick brown fox jumps over the lazy dog.";
-// convert the message into a char array
-char[] charMessage = str.ToCharArray();
+﻿// convert the message into a char array
 // Reverse the chars
-Array.Reverse(charMessage);
-int x = 0;
 // count the o's
-foreach (char i in charMessage) { if (i == 'o') { x++; } }
 // convert it back to a string
-string new_message = new String(charMessage);
 // print it out
+
+
+
+
+string str =  "The quick brown fox jumps over the lazy dog.";
+
+char[] charMessage = str.ToCharArray();
+Array.Reverse(charMessage);
+
+int x = 0;
+foreach (char i in charMessage)
+{ 
+    if (i == 'o') {
+        x++; 
+    } 
+}
+
+string new_message = new String(charMessage);
+
 Console.WriteLine(new_message);
 Console.WriteLine($"'o' appears {x} times.");
